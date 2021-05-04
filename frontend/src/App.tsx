5e592fa6b35e9535b0631e4d20215d24cdc84d1d
DenerThiago21 <denerthi.ibi@gmail.com/>
2021-05-03 21:43:46 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import Navbar from './components/navbar';
+import Footer from './components/footer';
+import DataTable from './components/dataTable';
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá pessoinhas</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá pessoinhas</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
